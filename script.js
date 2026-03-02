11d4270c65d6ebf4d8e26b211c94f674f398537b
raphaela <raphaelafelixdearaujo@gmail.com>
2026-03-02 16:15:47 -0300

    segundo terceiro

--- script.js
+++ script.js
@@ -33,8 +33,30 @@ document.addEventListener('keydown', function(){
 
 document.addEventListener('click', function(){
     // Mostra no console quando a tecla é pressionada
-    console.log('Tecla pressionada! chamando função pular()')
+    console.log('Click do mouse pressionado! chamando função pular()')
 
-    // Qual função deve ser chamada?
     pular();
+});
+
+console.log("===== INICIANDO O LOOP DO JOGO =====")
+console.log("Agora o jogo vai começar a verificar colisão")
+
+let loopDoJogo = setInterval(function(){
+    // offsetLeft: Distância do elemento até a borda esquerda da tela
+    // getComputeStyle = pega o estilo atual do elemento
+    // ----- > +window.getComputedStyle(mario)
+    // ----- > Pergunta ao navegador: "Qual é a posição atual do Mario na tela"
+    // ----- > .bottom
+    // Pega a distância do Mario (em pixels)
+    // ------ > .replace 
+    // ------ > Tira o px, deixando só o número: "120"
+    // ------- > +window, só o +
+    // ---- > Transforma o texto em "120" no número 120, para o JS fazer contas
+    let posicaoCano = cano.offsetLeft;
+
+    let posicaoMario = +window.getComputedStyle(mario).bottom.replace('px', '')
+
+    console.log('cano: ', posicaoCano, 'Mario: ', posicaoMario)
+
+    
 })
